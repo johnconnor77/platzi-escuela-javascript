07420c10bbbeafc19e1dbf3e8217cafcba60c34b
johnconnor77 <jfcalleh@unal.edu.co>
2020-09-25 20:00:07 -0500

    Test a promesas y async/await

--- curso-unit-testing-jest-react/jsbase/src/__test__/global.test.js
+++ curso-unit-testing-jest-react/jsbase/src/__test__/global.test.js
@@ -28,4 +28,29 @@ test('Probar un Callback', () => {
   })
 });
 
+const reverseString2 =  str => {
+  return new Promise ((resolve, reject) =>{
+    if (!str) {
+      reject(Error('Error'))
+    }
+    // split convierte en un arreglo, separando cada caracter
+    resolve(str.split("").reverse().join(""))
+  } );
+};
 
+test('Probar una promesa', () => {
+  return reverseString2('Hola')
+    .then(string => {
+      expect(string).toBe('aloH')
+    })
+});
+
+test('Probar asyn/ await', async () => {
+  const string = await reverseString2('hola');
+  expect(string).toBe('aloh')
+});
+
+afterEach (() => console.log('Después de cada prueba'));
+afterAll(() => console.log('Despues de todas las pruebas'));
+beforeEach(() => console.log('Antes de cada prueba'));
+beforeAll(() => console.log('Antes de todas las pruebas'))
