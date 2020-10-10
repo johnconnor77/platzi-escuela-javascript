e5ab5ef7af5b5076bec8afaf58b44096b43775e3
johnconnor77 <jfcalleh@unal.edu.co>
2020-10-10 04:54:52 -0500

    Promise: qué ocurre con las promesas al ser ejecutadas

---
+++ curso-asincronismo/src/promise/index.js
@@ -0,0 +1,36 @@
+const somethingWillHappen = () => {
+  return new Promise((resolve, reject) => {
+    if(true){
+      resolve('Hey!');
+    } else {
+      reject('Whoops!');
+    }
+  });
+};
+
+somethingWillHappen()
+  .then(response => console.log(response))
+  .catch(err => console.error(err))
+
+
+const somethingWillHappen2 = () => {
+  return new Promise((resolve, reject) => {
+    if(true){
+      setTimeout(()=>{
+        resolve('True');
+      }, 2000)
+    } else {
+      const error = new Error('Whoop!');
+      reject(error);
+    }
+  })
+}
+
+somethingWillHappen2()
+  .then(response => console.log(response))
+  .catch(error => console.log(error))
+
+
+Promise.all([somethingWillHappen(), somethingWillHappen2()])
+  .then(response => console.log('Array of results', response))
+  .catch(error => console.error(error))
